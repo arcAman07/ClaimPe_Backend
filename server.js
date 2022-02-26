f92d7e12fa3afb8a463ba2ab7130b5bfe54fcd76
Aman <aman.sharma2020b@vitstudent.ac.in>
2022-02-27 02:52:58 +0530

    make it better

--- server.js
+++ server.js
@@ -8,14 +8,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
 const insuranceRoutes = require("./routes/insuranceRoutes");
 const MONGO_URI = require("./config/config");
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = MONGO_URI;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  // const collection = client.db("insuranceDB").collection("insurances");
-  // perform actions on the collection object
-  client.close();
-});
+mongoose.connect(uri, {
+  })
+  .then(() =>
+    console.log("Database is connected!"))
+
+  .catch((err) => console.error(err))
+
 
 app.use(insuranceRoutes);
 app.listen(PORT, () => console.log("The server is running"));
