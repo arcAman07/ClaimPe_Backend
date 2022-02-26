b9ada7ffee38948a817106e63d6e537612d67208
Aman <aman.sharma2020b@vitstudent.ac.in>
2022-02-27 02:01:05 +0530

    Initial commit

--- database/db.js
+++
@@ -1,9 +0,0 @@
-const MONGO_URI = require("./config/config");
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = MONGO_URI;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("insuranceDB").collection("insurances");
-  // perform actions on the collection object
-  client.close();
-});
--- server.js
+++ server.js
@@ -5,13 +5,16 @@ const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT;
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json())
-
+app.use(bodyParser.json())
 const insuranceRoutes = require("./routes/insuranceRoutes");
-
-mongoose.connect("mongodb://localhost:27017/visionDB", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+const MONGO_URI = require("./config/config");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = MONGO_URI;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("insuranceDB").collection("insurances");
+  // perform actions on the collection object
+  client.close();
 });
 
 app.use(insuranceRoutes);
